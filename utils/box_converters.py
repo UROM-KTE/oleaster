387d171fd26790e8cb2a3f411ae7996e1d4adedb
ferenc-attila <attila.ferenc.80@gmail.com>
2023-07-02 11:26:38 +0200

    continue ...

--- utils/box_converters.py
+++ utils/box_converters.py
@@ -6,37 +6,42 @@ import tensorflow
 """
 
 
-def swap_xy(box):
+def swap_xy(boxes):
     """Swaps order the of x and y coordinates of the boxes.
+
     Arguments:
-      box: A tensor with shape `(num_boxes, 4)` representing bounding boxes.
+      boxes: A tensor with shape `(num_boxes, 4)` representing bounding boxes.
+
     Returns:
       swapped box with shape same as that of box.
     """
-    return tensorflow.stack([box[:, 1], box[:, 0], box[:, 3], box[:, 2]], axis=-1)
+    return tensorflow.stack([boxes[:, 1], boxes[:, 0], boxes[:, 3], boxes[:, 2]], axis=-1)
 
 
-def convert_to_xy_width_height(box):
+def convert_to_xy_width_height(boxes):
     """Changes the box format to center, width and height.
+
     Arguments:
-      box: A tensor of rank 2 or higher with a shape of `(..., num_boxes, 4)`
+      boxes: A tensor of rank 2 or higher with a shape of `(..., num_boxes, 4)`
         representing bounding boxes where each box is of the format
         `[xmin, ymin, xmax, ymax]`.
+
     Returns:
       converted box with shape same as that of box.
     """
     return tensorflow.concat(
-        [(box[..., :2] + box[..., 2:]) / 2.0, box[..., 2:] - box[..., :2]], axis=-1)
+        [(boxes[..., :2] + boxes[..., 2:]) / 2.0, boxes[..., 2:] - boxes[..., :2]], axis=-1)
 
 
-def convert_to_corners(box):
+def convert_to_corners(boxes):
     """Changes the box format to corner coordinates
+
     Arguments:
-        box: A tensor of rank 2 or higher with a shape of `(..., num_boxes, 4)`
+        boxes: A tensor of rank 2 or higher with a shape of `(..., num_boxes, 4)`
             representing bounding box where each box is of the format
             `[x, y, width, height]`.
     Returns:
         converted box with shape same as that of box.
     """
     return tensorflow.concat(
-        [box[..., :2] - box[..., 2:] / 2.0, box[..., :2] + box[..., 2:] / 2.0], axis=-1)
+        [boxes[..., :2] - boxes[..., 2:] / 2.0, boxes[..., :2] + boxes[..., 2:] / 2.0], axis=-1)
--- utils/pairwise_intersecting.py
+++ utils/pairwise_intersecting.py
@@ -1,8 +1,44 @@
+import numpy
 import tensorflow
+from matplotlib import pyplot
 
 import box_converters
 
 
-def compute_intersection_over_union(first_box, second_box):
-    first_boxes_corners = box_converters.convert_to_corners(first_box)
-    second_boxes_corners = box_converters.convert_to_corners(second_box)
+def compute_intersection_over_union(first_boxes, second_boxes):
+    """Computes pairwise IOU matrix for given two sets of boxes
+
+        Arguments:
+          first_boxes: A tensor with shape `(N, 4)` representing bounding boxes
+            where each box is of the format `[x, y, width, height]`.
+          second_boxes: A tensor with shape `(M, 4)` representing bounding boxes
+            where each box is of the format `[x, y, width, height]`.
+
+        Returns:
+          pairwise IOU matrix with shape `(N, M)`, where the value at ith row
+            jth column holds the IOU between ith box and jth box from
+            boxes1 and boxes2 respectively.
+    """
+    first_boxes_corners = box_converters.convert_to_corners(first_boxes)
+    second_boxes_corners = box_converters.convert_to_corners(second_boxes)
+    left_up = tensorflow.maximum(first_boxes_corners[:, None, :2], second_boxes_corners[second_boxes_corners[:, :2]])
+    right_down = tensorflow.minimum(first_boxes_corners[:, None, :2], second_boxes_corners[second_boxes_corners[:, :2]])
+    intersection = tensorflow.maximum(0.0, right_down - left_up)
+    intersection_area = intersection[:, :, 0] * intersection[:, :, 1]
+    first_boxes_area = first_boxes[:, 2] * first_boxes[:, 3]
+    second_boxes_area = second_boxes[:, 2] * second_boxes[:, 3]
+    union_area = tensorflow.maximum(
+        first_boxes_area[:, None] + second_boxes_area - intersection_area, 1e-8
+    )
+    return tensorflow.clip_by_value(intersection_area / union_area, 0.0, 1.0)
+
+
+def visualize_detections(
+        image, boxes, classes, scores, figure_size=(7, 7), linewidth=1, color=[0, 0, 1]
+):
+    image = numpy.array(image, dtype=numpy.uint8)
+    pyplot.figure(figsize=figure_size)
+    pyplot.axis("off")
+    pyplot.imshow(image)
+    ax = pyplot.gca()
+
